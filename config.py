f102d50ab3991b39df0f7c00a80dfa6a95f5c959
sunonamusic1 <simran25u7@gmail.com>
2025-05-28 06:28:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 170000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002657052717))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002259722835))
 
-# Get this value from  on Telegram by /id
+# Get this value from  on Telegram  /id
 OWNER_ID = int(getenv("OWNER_ID", 7861988028))
 
 ## Fill these variables if you're deploying on heroku.
